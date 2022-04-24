f45372417d94a0828e9a25c5ae05d90a1020df15
LEE <deed1515@naver.com>
2022-04-24 16:31:57 +0900

    init

---
+++ src/main/java/com/whiteship/reactive/WhiteShipReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.whiteship.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiteShipReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhiteShipReactiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/whiteship/reactive/WhiteShipReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whiteship.reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhiteShipReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
